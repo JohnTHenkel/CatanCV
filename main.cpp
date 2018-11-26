d5244577f5e44c437490c2bb9aa3958d7de8de1a
RafeMcAtee <45111292+RafeMcAtee@users.noreply.github.com>
2018-11-26 12:24:31 -0700

    added function to do surf search for tile pieces
    does not work well

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <cmath>
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include "opencv2/xfeatures2d.hpp"
 using namespace cv;
 using namespace std;
 
@@ -15,6 +17,9 @@ const double BOARD_AREA_MAX_THRESHOLD=700000*SCALE;
 int readDieRoll(Mat image);
 Mat isolateColor(Mat image,char color);
 Mat diffImage(Mat image, Mat backgroundImage);
+void detectEdges(Mat image);
+void findTiles(Mat image, const string& name);
+
 int main(int argc, char* argv[]){
 	//Mat image = imread("TestPhotos/Dice6.png");
 	Mat image = imread("TestPhotos/2018-11-07-162752.jpg");
@@ -38,12 +43,19 @@ int main(int argc, char* argv[]){
 	waitKey(0);
 	*/
 
+	/*
 	imshow("difference image",diffImage(image, backgroundImage));
 	waitKey(0);
+	*/
+
+	//detectEdges(backgroundImage);
 	
+	findTiles(backgroundImage,"Brick1.jpg");
 
 	return EXIT_SUCCESS;
 }
+
+
 int readDieRoll(Mat image){
 	Mat imageGray;
 	Mat imageBW;
@@ -208,4 +220,86 @@ Mat diffImage(Mat image, Mat backgroundImage){
     	}
 	//imshow("difference",diffImage);
 	return foregroundMask;
+}
+
+void detectEdges(Mat image){
+	Mat imageGray;
+	Mat detectedEdges;
+	cvtColor(image, imageGray, COLOR_BGR2GRAY);
+	blur(imageGray, detectedEdges, Size(3,3));
+	Canny(detectedEdges,detectedEdges, 30, 30*3, 3);
+	vector<Vec4i> lines;
+	int minLineLength = 170;
+	int maxLineGap = 10;
+	HoughLinesP(detectedEdges, lines, 1, CV_PI/180, 5, minLineLength, maxLineGap);
+	Mat houghEdges;
+	cvtColor(detectedEdges, houghEdges, COLOR_GRAY2BGR);
+	for(size_t i = 0; i < lines.size(); i++){
+    		line(houghEdges, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 3, LINE_AA);
+	}
+	imshow("source",image);
+	imshow("detected lines", houghEdges);
+	waitKey(0);
+}
+
+//REQUIRES xfeatures2d !!!
+void findTiles(Mat img_scene,const string& name){
+	using namespace cv::xfeatures2d;
+	string imagePath = "Templates/";
+	imagePath.append(name);
+	Mat img_object = imread(imagePath);
+	cvtColor(img_scene, img_scene, COLOR_BGR2GRAY);
+	cvtColor(img_object, img_object, COLOR_BGR2GRAY);
+	//extractChannel(img_object,img_object, 2);
+	//extractChannel(img_scene,img_scene, 2);
+	//resize tile template to aproximate same size as in image and blur to approximate resolution
+	resize(img_object, img_object, Size(), 0.1, 0.1);
+	blur(img_object, img_object, Size(4,4));
+	//detect keypoints
+	//int minHessian = 400;
+	int minHessian = 200;
+	Ptr<SURF> detector = SURF::create(minHessian);
+	vector<KeyPoint> keypoints_object, keypoints_scene;
+	Mat descriptors_object, descriptors_scene;
+	detector->detectAndCompute(img_object, noArray(), keypoints_object, descriptors_object);
+	detector->detectAndCompute(img_scene, noArray(), keypoints_scene, descriptors_scene);
+	//find matches
+	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    	vector< vector<DMatch> > knn_matches;
+    	matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+	//filter matches
+	//const float ratio_thresh = 0.75f;
+	const float ratio_thresh = 0.75f;
+    	vector<DMatch> good_matches;
+    	for (size_t i = 0; i < knn_matches.size(); i++)
+    	{
+        	if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        	{
+            		good_matches.push_back(knn_matches[i][0]);
+        	}
+    	}
+	//draw matches
+	 Mat img_matches;
+    	drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+	//localize object
+	vector<Point2f> obj;
+	vector<Point2f> scene;
+	for( size_t i = 0; i < good_matches.size(); i++ ){
+		obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+        	scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+	}
+	Mat tform = findHomography(obj, scene, RANSAC);
+	vector<Point2f> obj_corners(4);
+    	obj_corners[0] = Point2f(0, 0);
+    	obj_corners[1] = Point2f( (float)img_object.cols, 0 );
+    	obj_corners[2] = Point2f( (float)img_object.cols, (float)img_object.rows );
+    	obj_corners[3] = Point2f( 0, (float)img_object.rows );
+    	vector<Point2f> scene_corners(4);
+    	perspectiveTransform( obj_corners, scene_corners, tform);
+	line( img_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4 );
+    	line( img_matches, scene_corners[1] + Point2f((float)img_object.cols, 0),scene_corners[2] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    	line( img_matches, scene_corners[2] + Point2f((float)img_object.cols, 0),scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    	line( img_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+	imshow("Good Matches & Object detection", img_matches );
+    	waitKey();
 }
